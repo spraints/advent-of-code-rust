e4c2b72259271ad8841d918ac87955bf8e83ca44
Matt Burke <spraints@gmail.com>
2022-12-02 00:15:29 -0500

    clippy

--- src/years/y2022/d2.rs
+++ src/years/y2022/d2.rs
@@ -1,20 +1,19 @@
-use std::{collections::BinaryHeap, fmt::Display};
+use std::fmt::Display;
 
 pub fn part1(input: String) -> anyhow::Result<Box<dyn Display>> {
     let mut score: u64 = 0;
     for line in input.lines() {
-        let opp = line.chars().nth(0).unwrap();
-        let me = line.chars().nth(2).unwrap();
+        let (opp, me) = line.split_once(' ').unwrap();
         score += match (opp, me) {
-            ('A', 'X') => 1 + 3,
-            ('B', 'X') => 1 + 0,
-            ('C', 'X') => 1 + 6,
-            ('A', 'Y') => 2 + 6,
-            ('B', 'Y') => 2 + 3,
-            ('C', 'Y') => 2 + 0,
-            ('A', 'Z') => 3 + 0,
-            ('B', 'Z') => 3 + 6,
-            ('C', 'Z') => 3 + 3,
+            ("A", "X") => 1 + 3,
+            ("B", "X") => 1, // + 0,
+            ("C", "X") => 1 + 6,
+            ("A", "Y") => 2 + 6,
+            ("B", "Y") => 2 + 3,
+            ("C", "Y") => 2, // + 0,
+            ("A", "Z") => 3, // + 0,
+            ("B", "Z") => 3 + 6,
+            ("C", "Z") => 3 + 3,
             _ => panic!("illegal {}", line),
         };
     }
@@ -24,8 +23,7 @@ pub fn part1(input: String) -> anyhow::Result<Box<dyn Display>> {
 pub fn part2(input: String) -> anyhow::Result<Box<dyn Display>> {
     let mut score: u64 = 0;
     for line in input.lines() {
-        let opp = line.chars().nth(0).unwrap();
-        let me = line.chars().nth(2).unwrap();
+        let (opp, me) = line.split_once(' ').unwrap();
         score += match (opp, me) {
             // A = opp Rock
             // B = opp Paper
@@ -33,15 +31,15 @@ pub fn part2(input: String) -> anyhow::Result<Box<dyn Display>> {
             // X = Lose
             // Y = Draw
             // Z = Win
-            ('A', 'X') => 3 + 0,
-            ('B', 'X') => 1 + 0,
-            ('C', 'X') => 2 + 0,
-            ('A', 'Y') => 1 + 3,
-            ('B', 'Y') => 2 + 3,
-            ('C', 'Y') => 3 + 3,
-            ('A', 'Z') => 2 + 6,
-            ('B', 'Z') => 3 + 6,
-            ('C', 'Z') => 1 + 6,
+            ("A", "X") => 3, // + 0,
+            ("B", "X") => 1, // + 0,
+            ("C", "X") => 2, // + 0,
+            ("A", "Y") => 1 + 3,
+            ("B", "Y") => 2 + 3,
+            ("C", "Y") => 3 + 3,
+            ("A", "Z") => 2 + 6,
+            ("B", "Z") => 3 + 6,
+            ("C", "Z") => 1 + 6,
             _ => panic!("illegal {}", line),
         };
     }
