8bb047d9f5fb37d2ec5bd67098c912d53de1c29a
Matt Burke <spraints@gmail.com>
2022-12-04 00:14:11 -0500

    solved

--- src/years/y2022/d4.rs
+++ src/years/y2022/d4.rs
@@ -1,11 +1,45 @@
 use std::fmt::Display;
 
 pub fn part1(input: String) -> Box<dyn Display> {
-    Box::new(input)
+    fn parse(s: &str) -> (u8, u8, u8, u8) {
+        let (a, b) = s.split_once(",").unwrap();
+        let (a1, b1) = a.split_once("-").unwrap();
+        let (a2, b2) = b.split_once("-").unwrap();
+        (
+            a1.parse().unwrap(),
+            b1.parse().unwrap(),
+            a2.parse().unwrap(),
+            b2.parse().unwrap(),
+        )
+    }
+    fn completely_contains(x: &(u8, u8, u8, u8)) -> bool {
+        let (a1, b1, a2, b2) = x;
+        let res = (a1 <= a2 && b1 >= b2) || (a2 <= a1 && b2 >= b1);
+        //println!("{:?} => {:?}", x, res);
+        res
+    }
+    Box::new(input.lines().map(parse).filter(completely_contains).count())
 }
 
 pub fn part2(input: String) -> Box<dyn Display> {
-    Box::new(input)
+    fn parse(s: &str) -> (u8, u8, u8, u8) {
+        let (a, b) = s.split_once(",").unwrap();
+        let (a1, b1) = a.split_once("-").unwrap();
+        let (a2, b2) = b.split_once("-").unwrap();
+        (
+            a1.parse().unwrap(),
+            b1.parse().unwrap(),
+            a2.parse().unwrap(),
+            b2.parse().unwrap(),
+        )
+    }
+    fn overlaps(x: &(u8, u8, u8, u8)) -> bool {
+        let (a1, b1, a2, b2) = x;
+        let res = (a1 <= a2 && b1 >= a2) || (a2 <= a1 && b2 >= a1);
+        //println!("{:?} => {:?}", x, res);
+        res
+    }
+    Box::new(input.lines().map(parse).filter(overlaps).count())
 }
 
 #[cfg(test)]
@@ -13,15 +47,21 @@ mod test {
     use super::*;
     use crate::test::*;
 
-    const EX: &str = r"";
+    const EX: &str = r"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+";
 
     #[test]
     fn part1_example() {
-        dotest(EX, EX, part1);
+        dotest(2, EX, part1);
     }
 
     #[test]
     fn part2_example() {
-        dotest(EX, EX, part2);
+        dotest(4, EX, part2);
     }
 }
