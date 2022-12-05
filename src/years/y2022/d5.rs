e67f50b57c2e4ebf15431073d7ea511d4019cd97
Matt Burke <spraints@gmail.com>
2022-12-05 09:48:14 -0500

    part 1

--- src/years/y2022/d5.rs
+++ src/years/y2022/d5.rs
@@ -1,11 +1,119 @@
 use std::fmt::Display;
 
-pub fn part1(input: String) -> Box<dyn Display> {
-    Box::new(input)
+type Stack = Vec<char>;
+struct Move {
+    count: usize,
+    from: usize,
+    to: usize,
 }
 
-pub fn part2(input: String) -> Box<dyn Display> {
-    Box::new(input)
+pub fn part1(input: String) -> Box<dyn Display> {
+    let (mut stacks, moves) = parse(&input);
+    //show_stacks(&stacks);
+    for m in moves {
+        //println!("{}", m);
+        for _ in 0..m.count {
+            let moved = stacks[m.from].pop().unwrap();
+            stacks[m.to].push(moved);
+        }
+        //show_stacks(&stacks);
+    }
+    Box::new(
+        stacks
+            .into_iter()
+            .map(|mut s| s.pop().unwrap())
+            .collect::<String>(),
+    )
+}
+
+pub fn part2(_input: String) -> Box<dyn Display> {
+    Box::new("todo2")
+}
+
+fn show_stacks(stacks: &Vec<Stack>) {
+    let mut i = stacks.iter().map(|s| s.len()).max().unwrap();
+    while i > 0 {
+        i -= 1;
+        for stack in stacks {
+            match stack.get(i) {
+                None => print!("    "),
+                Some(c) => print!("[{}] ", c),
+            };
+        }
+        println!("");
+    }
+    for i in 0..stacks.len() {
+        print!(" {}  ", i + 1);
+    }
+    println!("");
+}
+
+impl std::fmt::Display for Move {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "move {} from {} to {}",
+            self.count,
+            self.from + 1,
+            self.to + 1
+        )
+    }
+}
+
+fn parse(s: &str) -> (Vec<Stack>, Vec<Move>) {
+    fn update_stacks(stacks: &mut Vec<Stack>, line: &str) {
+        let mut line = line.chars();
+        let mut i = 0;
+        loop {
+            match line.next() {
+                Some('[') => {
+                    let c = line.next().unwrap();
+                    if i >= stacks.len() {
+                        stacks.resize(i + 1, Default::default());
+                    }
+                    stacks[i].insert(0, c);
+                    for _ in 0..2 {
+                        if let None = line.next() {
+                            return;
+                        }
+                    }
+                }
+                Some(' ') => {
+                    for _ in 0..3 {
+                        if let None = line.next() {
+                            return;
+                        }
+                    }
+                }
+                _ => return,
+            };
+
+            i += 1;
+        }
+    }
+    fn parse_move(line: &str) -> Move {
+        let mut words = line.split(' ');
+        words.next().unwrap();
+        let count = words.next().unwrap().parse().unwrap();
+        words.next().unwrap();
+        let from = words.next().unwrap().parse::<usize>().unwrap() - 1;
+        words.next().unwrap();
+        let to = words.next().unwrap().parse::<usize>().unwrap() - 1;
+        Move { count, from, to }
+    }
+    let mut stacks = Vec::new();
+    let mut moves = Vec::new();
+    let mut state = 0;
+    for line in s.lines() {
+        match (state, line) {
+            (0, l) if l.starts_with(" 1 ") => state = 1,
+            (0, l) => update_stacks(&mut stacks, l),
+            (1, _) => state = 2,
+            (2, l) => moves.push(parse_move(l)),
+            _ => unreachable!(),
+        };
+    }
+    (stacks, moves)
 }
 
 #[cfg(test)]
@@ -13,15 +121,23 @@ mod test {
     use super::*;
     use crate::test::*;
 
-    const EX: &str = r"";
+    const EX: &str = r"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2";
 
     #[test]
     fn part1_example() {
-        dotest(EX, EX, part1);
+        dotest("CMZ", EX, part1);
     }
 
     #[test]
     fn part2_example() {
-        dotest(EX, EX, part2);
+        dotest("todo2", EX, part2);
     }
 }
