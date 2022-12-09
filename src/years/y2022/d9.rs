64665c8305e7b8a8473430ae0a074ada50408fcd
Matt Burke <spraints@gmail.com>
2022-12-09 08:44:40 -0500

    clippy

--- src/years/y2022/d9.rs
+++ src/years/y2022/d9.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashSet, fmt::Display};
+use std::{cmp::Ordering, collections::HashSet, fmt::Display};
 
 pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
     let (mut hx, mut hy) = (0, 0);
@@ -39,7 +39,7 @@ pub fn part2(input: String, vis: bool) -> Box<dyn Display> {
     for line in input.lines() {
         let ((dx, dy), steps) = parse(line);
         for _ in 0..steps {
-            let (mut hx, mut hy) = positions[0].clone();
+            let (mut hx, mut hy) = positions[0];
             if vis {
                 println!("head: ({},{}) + ({},{})", hx, hy, dx, dy);
             }
@@ -49,53 +49,52 @@ pub fn part2(input: String, vis: bool) -> Box<dyn Display> {
             if vis {
                 println!("  [{}] ({},{})", 0, hx, hy);
             }
-            for i in 1..10 {
-                let (mut tx, mut ty) = positions[i].clone();
+            for (i, pos) in positions.iter_mut().enumerate().skip(1) {
+                let (mut tx, mut ty) = pos;
                 if ty - hy > 1 {
-                    if hx > tx {
-                        tx += 1
-                    } else if tx > hx {
-                        tx -= 1
-                    }
                     ty -= 1;
+                    tx += match tx.cmp(&hx) {
+                        Ordering::Less => 1,
+                        Ordering::Equal => 0,
+                        Ordering::Greater => -1,
+                    };
                 } else if hy - ty > 1 {
-                    if hx > tx {
-                        tx += 1
-                    } else if tx > hx {
-                        tx -= 1
-                    }
                     ty += 1;
+                    tx += match tx.cmp(&hx) {
+                        Ordering::Less => 1,
+                        Ordering::Equal => 0,
+                        Ordering::Greater => -1,
+                    };
                 } else if hx - tx > 1 {
-                    if ty > hy {
-                        ty -= 1;
-                    } else if hy > ty {
-                        ty += 1;
-                    }
                     tx += 1;
+                    ty += match ty.cmp(&hy) {
+                        Ordering::Less => 1,
+                        Ordering::Equal => 0,
+                        Ordering::Greater => -1,
+                    };
                 } else if tx - hx > 1 {
-                    if ty > hy {
-                        ty -= 1;
-                    } else if hy > ty {
-                        ty += 1;
-                    }
                     tx -= 1;
+                    ty += match ty.cmp(&hy) {
+                        Ordering::Less => 1,
+                        Ordering::Equal => 0,
+                        Ordering::Greater => -1,
+                    };
                 } else {
                     break;
                 }
                 if vis {
                     println!("  [{}] ({},{})", i, tx, ty);
                 }
-                positions[i] = (tx, ty);
+                *pos = (tx, ty);
                 hx = tx;
                 hy = ty;
             }
-            visited.insert(positions[9].clone());
+            visited.insert(positions[9]);
         }
         if vis {
             println!("{} => {:?}", line, positions);
         }
     }
-    // 2528 is too high
     Box::new(visited.len())
 }
 
@@ -136,4 +135,6 @@ R 17
 D 10
 L 25
 U 20", part2 => 36);
+
+    // test input, part1 => 6087, part2 => 2493
 }
