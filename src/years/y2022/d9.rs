31f495fd3c24dbeaa6ef107e5eb306abca874c36
Matt Burke <spraints@gmail.com>
2022-12-09 08:34:08 -0500

    got it

--- src/years/y2022/d9.rs
+++ src/years/y2022/d9.rs
@@ -32,8 +32,102 @@ pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
     Box::new(visited.len())
 }
 
-pub fn part2(_input: String, _vis: bool) -> Box<dyn Display> {
-    Box::new("todo")
+pub fn part2(input: String, vis: bool) -> Box<dyn Display> {
+    let mut positions = vec![(0, 0); 10];
+    let mut visited = HashSet::new();
+    visited.insert((0, 0));
+    for line in input.lines() {
+        let ((dx, dy), steps) = parse(line);
+        for _ in 0..steps {
+            let (mut hx, mut hy) = positions[0].clone();
+            println!("head: ({},{}) + ({},{})", hx, hy, dx, dy);
+            hx += dx;
+            hy += dy;
+            positions[0] = (hx, hy);
+            for i in 1..10 {
+                let (mut tx, mut ty) = positions[i].clone();
+                if ty - hy > 1 {
+                    if vis {
+                        println!(" [{}] ({},{}) catch up to ({},{})", i, tx, ty, hx, hy);
+                    }
+                    if hx > tx {
+                        tx += 1
+                    } else if tx > hx {
+                        tx -= 1
+                    }
+                    ty -= 1;
+                } else if hy - ty > 1 {
+                    if vis {
+                        println!(" [{}] ({},{}) catch up to ({},{})", i, tx, ty, hx, hy);
+                    }
+                    if hx > tx {
+                        tx += 1
+                    } else if tx > hx {
+                        tx -= 1
+                    }
+                    ty += 1;
+                } else if hx - tx > 1 {
+                    if vis {
+                        println!(" [{}] ({},{}) catch up to ({},{})", i, tx, ty, hx, hy);
+                    }
+                    if ty > hy {
+                        ty -= 1;
+                    } else if hy > ty {
+                        ty += 1;
+                    }
+                    tx += 1;
+                } else if tx - hx > 1 {
+                    if vis {
+                        println!(" [{}] ({},{}) catch up to ({},{})", i, tx, ty, hx, hy);
+                    }
+                    if ty > hy {
+                        ty -= 1;
+                    } else if hy > ty {
+                        ty += 1;
+                    }
+                    tx -= 1;
+                }
+                if vis {
+                    println!("  [{}] ({},{})", i, tx, ty);
+                }
+                positions[i] = (tx, ty);
+                hx = tx;
+                hy = ty;
+            }
+            visited.insert(positions[9].clone());
+        }
+        if vis {
+            println!("{} => {:?}", line, positions);
+            /*
+            let xs: Vec<isize> = positions.iter().map(|(x, _)| x).copied().collect();
+            let ys: Vec<isize> = positions.iter().map(|(_, y)| y).copied().collect();
+            let minx = *xs.iter().min().unwrap();
+            let maxx = *xs.iter().max().unwrap();
+            let miny = *ys.iter().min().unwrap();
+            let maxy = *ys.iter().max().unwrap();
+            let mut grid = vec![
+                vec!['.'; (maxy - miny + 1).try_into().unwrap()];
+                (maxx - minx + 1).try_into().unwrap()
+            ];
+            for (i, (x, y)) in positions.iter().enumerate() {
+                let label = match i as u32 {
+                    0 => 'H',
+                    n => unsafe { char::from_u32_unchecked(30 + n) },
+                };
+                let line: &mut Vec<char> = &mut grid[(x - minx) as usize];
+                line[(y - miny) as usize] = label;
+            }
+            for y in (miny..maxy).rev() {
+                for x in minx..maxx {
+                    print!("{}", grid[(x - minx) as usize][(y - miny) as usize]);
+                }
+                println!();
+            }
+            */
+        }
+    }
+    // 2528 is too high
+    Box::new(visited.len())
 }
 
 fn parse(line: &str) -> ((isize, isize), usize) {
@@ -63,5 +157,14 @@ D 1
 L 5
 R 2",
         part1 => 13,
-        part2 => "todo");
+        part2 => 1);
+
+    crate::test::aoc_test!(longer, r"R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20", part2 => 35);
 }
