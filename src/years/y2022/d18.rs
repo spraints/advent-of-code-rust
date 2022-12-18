44903f1fc711f86db8e8fc02437de5eb43aaca51
Matt Burke <spraints@gmail.com>
2022-12-18 17:32:50 -0500

    part 2, not right

--- src/years/y2022/d18.rs
+++ src/years/y2022/d18.rs
@@ -17,15 +17,6 @@ pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
                 println!("  - cover {} ({}, {}, {})", c, x, y, z);
             }
         }
-        fn add_side(exposed: &mut HashMap<P, usize>, covered: &HashSet<P>, vis: bool, p: P) {
-            if !covered.contains(&p) {
-                if vis {
-                    println!("  + expose {:?}", p);
-                }
-                let c = exposed.entry(p).or_insert(0);
-                *c += 1;
-            }
-        }
         add_side(&mut exposed, &covered, vis, (x + 1, y, z));
         add_side(&mut exposed, &covered, vis, (x - 1, y, z));
         add_side(&mut exposed, &covered, vis, (x, y + 1, z));
@@ -42,8 +33,51 @@ pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
     Box::new(exposed.values().sum::<usize>())
 }
 
-pub fn part2(_input: String, _vis: bool) -> Box<dyn Display> {
-    Box::new("todo")
+pub fn part2(input: String, vis: bool) -> Box<dyn Display> {
+    let covered: HashSet<P> = input.lines().map(parse).collect();
+    let (maxx, maxy, maxz) = covered
+        .iter()
+        .copied()
+        .reduce(|(ax, ay, az), (bx, by, bz)| (ax.max(bx), ay.max(by), az.max(bz)))
+        .unwrap();
+    if vis {
+        println!("max: ({},{},{})", maxx, maxy, maxz);
+    }
+
+    fn e<I: Iterator<Item = C>, F: Fn(C) -> P>(covered: &HashSet<P>, r: I, next: F) -> u16 {
+        for c in r {
+            if covered.contains(&next(c)) {
+                return 0;
+            }
+        }
+        1
+    }
+
+    let mut exposed = 0;
+    for p in &covered {
+        let (x, y, z) = p.clone();
+        let before = exposed;
+        exposed += e(&covered, 0..x, |x| (x, y, z));
+        exposed += e(&covered, (x + 1)..=maxx, |x| (x, y, z));
+        exposed += e(&covered, 0..y, |y| (x, y, z));
+        exposed += e(&covered, (y + 1)..=maxy, |y| (x, y, z));
+        exposed += e(&covered, 0..z, |z| (x, y, z));
+        exposed += e(&covered, (z + 1)..=maxz, |z| (x, y, z));
+        if vis {
+            println!("{:?} => {}", p, exposed - before);
+        }
+    }
+    Box::new(exposed)
+}
+
+fn add_side(exposed: &mut HashMap<P, usize>, covered: &HashSet<P>, vis: bool, p: P) {
+    if !covered.contains(&p) {
+        if vis {
+            println!("  + expose {:?}", p);
+        }
+        let c = exposed.entry(p).or_insert(0);
+        *c += 1;
+    }
 }
 
 type P = (C, C, C);
@@ -78,5 +112,5 @@ mod test {
 2,1,5
 2,3,5",
             part1 => 64,
-            part2 => "todo");
+            part2 => 58);
 }
