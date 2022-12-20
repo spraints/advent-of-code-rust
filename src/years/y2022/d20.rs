21fccd6b64a7c124c5e8e444fa63065ba8b3151d
Matt Burke <spraints@gmail.com>
2022-12-20 13:56:21 -0500

    try part 2 naively, but it's too slow of course

--- src/years/y2022/d20.rs
+++ src/years/y2022/d20.rs
@@ -2,21 +2,54 @@ use std::fmt::Display;
 
 // NOT -6697
 pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
-    let mut values: Vec<(usize, i32)> = input
+    let mut values: Vec<(usize, i64)> = input
         .lines()
         .map(|s| s.parse().unwrap())
         .enumerate()
         .collect();
+    mix(&mut values, vis);
+
+    let zero = values.iter().position(|&(_, v)| v == 0).unwrap();
+    let a = values[(zero + 1000) % values.len()].1;
+    let b = values[(zero + 2000) % values.len()].1;
+    let c = values[(zero + 3000) % values.len()].1;
+    Box::new(a + b + c)
+}
+
+pub fn part2(input: String, vis: bool) -> Box<dyn Display> {
+    const KEY: i64 = 811589153;
+    let mut values: Vec<(usize, i64)> = input
+        .lines()
+        .map(|s| KEY * s.parse::<i64>().unwrap())
+        .enumerate()
+        .collect();
+
+    if vis {
+        println!("Initial arrangement:");
+        print_values(&values, Some(4));
+    }
+
+    for i in 0..10 {
+        mix(&mut values, false);
+        if vis {
+            println!("After round {}:", i + 1);
+            print_values(&values, Some(4));
+        }
+    }
+
+    let zero = values.iter().position(|&(_, v)| v == 0).unwrap();
+    let a = values[(zero + 1000) % values.len()].1;
+    let b = values[(zero + 2000) % values.len()].1;
+    let c = values[(zero + 3000) % values.len()].1;
+    Box::new(a + b + c)
+}
+
+fn mix(values: &mut Vec<(usize, i64)>, vis: bool) {
     if vis {
         println!("Initial arrangement of {} items:", values.len());
-        let mut sep = "";
-        for (_, v) in &values {
-            print!("{}{}", sep, v);
-            sep = ", ";
-        }
-        println!();
+        print_values(values, None);
     }
-    let len = values.len() as i32;
+    let len = values.len() as i64;
     for order in 0..values.len() {
         let (origpos, (_, val)) = values
             .iter()
@@ -25,7 +58,11 @@ pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
             .unwrap();
         let origpos = origpos;
         let val = *val;
-        let mut pos = origpos as i32;
+        let mut pos = origpos as i64;
+        let guess_newpos = match origpos as i64 + val {
+            x if x < 0 => (x + len * (x.abs() / len) + len) % len,
+            x => x % len,
+        };
         if val != 0 {
             let offset = val / val.abs();
             for _ in 0..val.abs() {
@@ -40,26 +77,27 @@ pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
                 pos = newpos;
             }
         }
+        assert_eq!(guess_newpos, pos);
         if vis {
             println!("moved {} from [{}] to [{}]", val, origpos, pos);
-            let mut sep = "";
-            for (_, v) in &values {
-                print!("{}{}", sep, v);
-                sep = ", ";
-            }
-            println!();
+            print_values(values, None);
         }
     }
-
-    let zero = values.iter().position(|&(_, v)| v == 0).unwrap();
-    let a = values[(zero + 1000) % values.len()].1;
-    let b = values[(zero + 2000) % values.len()].1;
-    let c = values[(zero + 3000) % values.len()].1;
-    Box::new(a + b + c)
 }
 
-pub fn part2(input: String, vis: bool) -> Box<dyn Display> {
-    Box::new("todo")
+fn print_values(mut values: &[(usize, i64)], cap: Option<usize>) {
+    if let Some(cap) = cap {
+        values = &values[0..cap];
+    }
+    let mut sep = "";
+    for (_, v) in values {
+        print!("{}{}", sep, v);
+        sep = ", ";
+    }
+    if cap.is_some() {
+        print!("{}...", sep);
+    }
+    println!();
 }
 
 #[cfg(test)]
@@ -79,6 +117,6 @@ mod test {
 -2
 0
 4",
-        part1 => 3,
-        part2 => "todo");
+        part1 => 3/*,
+        part2 => 1623178306*/);
 }
