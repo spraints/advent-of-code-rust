c504e0fc5843e891eb9c49a4877e21ed932ff505
Matt Burke <spraints@gmail.com>
2022-12-20 22:01:38 -0500

    refactor, add test for mix

--- src/years/y2022/d20.rs
+++ src/years/y2022/d20.rs
@@ -1,67 +1,51 @@
 use std::fmt::Display;
 
-// NOT -6697
 // YES 16533
+// NOT -6697
 pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
-    let mut values: Vec<(usize, i64)> = input
+    let mut values: Vec<(usize, i32)> = input
         .lines()
         .map(|s| s.parse().unwrap())
         .enumerate()
         .collect();
+
     mix(&mut values, vis);
 
-    let zero = values.iter().position(|&(_, v)| v == 0).unwrap();
-    let a = values[(zero + 1000) % values.len()].1;
-    let b = values[(zero + 2000) % values.len()].1;
-    let c = values[(zero + 3000) % values.len()].1;
-    Box::new(a + b + c)
+    Box::new(score(values))
 }
 
 pub fn part2(input: String, vis: bool) -> Box<dyn Display> {
-    const KEY: i64 = 811589153;
-    let mut values: Vec<(usize, i64)> = input
-        .lines()
-        .map(|s| KEY * s.parse::<i64>().unwrap())
-        .enumerate()
-        .collect();
+    Box::new("todo")
+}
 
-    if vis {
-        println!("Initial arrangement:");
-        print_values(&values, Some(4));
-    }
-
-    for i in 0..10 {
-        if i == 0 {
-            return Box::new("BOOOM");
-        }
-        mix(&mut values, false);
-        if vis {
-            println!("After round {}:", i + 1);
-            print_values(&values, Some(4));
-        }
-    }
-
+fn score(values: Vec<(usize, i32)>) -> i32 {
     let zero = values.iter().position(|&(_, v)| v == 0).unwrap();
     let a = values[(zero + 1000) % values.len()].1;
     let b = values[(zero + 2000) % values.len()].1;
     let c = values[(zero + 3000) % values.len()].1;
-    Box::new(a + b + c)
+    a + b + c
 }
 
-fn mix(values: &mut Vec<(usize, i64)>, vis: bool) {
+fn mix(values: &mut Vec<(usize, i32)>, vis: bool) {
     if vis {
         println!("Initial arrangement of {} items:", values.len());
-        print_values(values, None);
+        let mut sep = "";
+        for (_, v) in values.iter() {
+            print!("{}{}", sep, v);
+            sep = ", ";
+        }
+        println!();
     }
-    let len = values.len() as i64;
+    let len = values.len() as i32;
     for order in 0..values.len() {
         let (origpos, (_, val)) = values
             .iter()
             .enumerate()
             .find(|(_, (i, _))| *i == order)
             .unwrap();
+        let origpos = origpos;
         let val = *val;
-        let mut pos = origpos as i64;
+        let mut pos = origpos as i32;
         if val != 0 {
             let offset = val / val.abs();
             for _ in 0..val.abs() {
@@ -78,27 +62,14 @@ fn mix(values: &mut Vec<(usize, i64)>, vis: bool) {
         }
         if vis {
             println!("moved {} from [{}] to [{}]", val, origpos, pos);
-            print_values(values, None);
-        }
-        if vis {
+            let mut sep = "";
+            for (_, v) in values.iter() {
+                print!("{}{}", sep, v);
+                sep = ", ";
+            }
             println!();
         }
     }
-}
-
-fn print_values(mut values: &[(usize, i64)], cap: Option<usize>) {
-    if let Some(cap) = cap {
-        values = &values[0..cap];
-    }
-    let mut sep = "";
-    for (_, v) in values {
-        print!("{}{}", sep, v);
-        sep = ", ";
-    }
-    if cap.is_some() {
-        print!("{}...", sep);
-    }
-    println!();
 }
 
 #[cfg(test)]
@@ -113,11 +84,32 @@ mod test {
 
     crate::test::aoc_test!(example, r"1
 2
--10
+-3
 3
 -2
 0
-11",
+4",
         part1 => 3,
-        part2 => 1623178306);
+        part2 => "todo");
+
+    fn mix(values: Vec<i32>) -> Vec<i32> {
+        let mut values = values.into_iter().enumerate().collect();
+        super::mix(&mut values, false);
+        values.into_iter().map(|(_, v)| v).collect()
+    }
+
+    #[test]
+    fn test_mix() {
+        assert_eq!(vec![0, 0, 0, 1, 0], mix(vec![0, 0, 1, 0, 0]));
+        assert_eq!(vec![0, -1, 0, 0, 0], mix(vec![0, 0, -1, 0, 0]));
+
+        assert_eq!(vec![0, 4, 0, 0, 0], mix(vec![0, 0, 4, 0, 0]));
+        assert_eq!(vec![0, 0, 0, -4, 0], mix(vec![0, 0, -4, 0, 0]));
+
+        assert_eq!(vec![0, 0, 0, 6, 0], mix(vec![0, 0, 6, 0, 0]));
+        assert_eq!(vec![0, -6, 0, 0, 0], mix(vec![0, 0, -6, 0, 0]));
+
+        assert_eq!(vec![0, 9, 0, 0, 0], mix(vec![0, 0, 9, 0, 0]));
+        assert_eq!(vec![0, 0, 0, -9, 0], mix(vec![0, 0, -9, 0, 0]));
+    }
 }
