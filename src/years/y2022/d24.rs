a4789c8b6f26fe9d7422b1b500b442a41ccfe779
Matt Burke <spraints@gmail.com>
2022-12-28 17:40:09 -0500

    part 1 example works

--- src/years/y2022/d24.rs
+++ src/years/y2022/d24.rs
@@ -1,4 +1,5 @@
 use std::{
+    cmp::Reverse,
     collections::{BinaryHeap, HashSet},
     fmt::Display,
 };
@@ -6,6 +7,9 @@ use std::{
 pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
     let board = parse_input(&input);
     let you_start = (0, board[0].iter().position(|sq| *sq == EMPTY).unwrap());
+    let bottom_row = board.len() - 1;
+
+    const MOVES: [(isize, isize); 5] = [(0, 0), (1, 0), (-1, 0), (0, 1), (0, -1)];
 
     if vis {
         println!("Initial state:");
@@ -13,39 +17,59 @@ pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
 
         let you = (you_start.0 + 1, you_start.1);
         println!();
-        println!("Minute 1:");
-        show_state(&board, &you, 1);
+        println!("Minute 1 (after waiting):");
+        show_state(&board, &you_start, 1);
     }
 
     let mut tried = HashSet::new();
     let mut todo = BinaryHeap::new();
-    todo.push(State {
+    todo.push(Reverse(State {
         elapsed: 0,
         pos: you_start,
-    });
+    }));
 
-    while let Some(st) = todo.pop() {
+    while let Some(Reverse(st)) = todo.pop() {
+        if vis {
+            println!("try {:?}", st);
+        }
         let State { elapsed, pos } = st;
         if tried.contains(&(pos, elapsed)) {
             continue;
         }
         tried.insert((pos, elapsed));
 
-        todo!("try waiting, if possible");
-        todo!("try moving down, if possible");
-        todo!("try moving up, if possible");
-        todo!("try moving left, if possible");
-        todo!("try moving right, if possible");
+        if pos.0 == bottom_row {
+            return Box::new(elapsed);
+        }
+
+        let elapsed = elapsed + 1;
+        let (r, c) = (pos.0 as isize, pos.1 as isize);
+        for (dr, dc) in &MOVES {
+            let nr = r + dr;
+            let nc = c + dc;
+            if nr >= 0 && nc >= 0 {
+                let pos = (nr as usize, nc as usize);
+                if pos.0 < board.len() && pos.1 <= board[0].len() {
+                    if is_empty(&board, elapsed, pos) {
+                        let next = State { pos, elapsed };
+                        if vis {
+                            println!(" -> {:?}", next);
+                        }
+                        todo.push(Reverse(next));
+                    }
+                }
+            }
+        }
     }
 
-    Box::new("todo")
+    unreachable!()
 }
 
 pub fn part2(input: String, vis: bool) -> Box<dyn Display> {
     Box::new("todo")
 }
 
-#[derive(PartialOrd, Ord, PartialEq, Eq)]
+#[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
 struct State {
     elapsed: usize,
     pos: Coord,
@@ -76,6 +100,50 @@ fn show_state(board: &Board, you: &Coord, elapsed: usize) {
             print!("{}", render(board, you, elapsed, (r, c)));
         }
         println!();
+    }
+}
+
+fn is_empty(board: &Board, elapsed: usize, pos: Coord) -> bool {
+    get(board, elapsed, pos) == EMPTY
+}
+
+fn get(board: &Board, elapsed: usize, pos: Coord) -> char {
+    let (r, c) = pos;
+
+    if board[r][c] == WALL {
+        return WALL;
+    }
+
+    if r == 0 || c == 0 || r + 1 == board.len() || c + 1 == board[0].len() {
+        assert!(board[r][c] == EMPTY);
+        return EMPTY;
+    }
+
+    let brows = board.len() - 2;
+    let bcols = board[0].len() - 2;
+    let roff = elapsed % brows;
+    let coff = elapsed % bcols;
+
+    let is_up = board[(r + roff - 1) % brows + 1][c] == UP;
+    let is_down = board[(r + brows - roff - 1) % brows + 1][c] == DOWN;
+    let is_left = board[r][(c + coff - 1) % bcols + 1] == LEFT;
+    let is_right = board[r][(c + bcols - coff - 1) % bcols + 1] == RIGHT;
+
+    match (is_up, is_down, is_left, is_right) {
+        (false, false, false, false) => EMPTY,
+        (true, false, false, false) => UP,
+        (false, true, false, false) => DOWN,
+        (false, false, true, false) => LEFT,
+        (false, false, false, true) => RIGHT,
+        (a, b, c, d) => match a as u8 + b as u8 + c as u8 + d as u8 {
+            2 => '2',
+            3 => '3',
+            4 => '4',
+            n => unreachable!(
+                "up:{} down:{} left:{} right:{} -> count={}",
+                is_up, is_down, is_left, is_right, n
+            ),
+        },
     }
 }
 
