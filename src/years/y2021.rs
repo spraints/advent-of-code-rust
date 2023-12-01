3b14b609a54c54dad16fade2baf38d0a399676a5
Matt Burke <spraints@gmail.com>
2023-11-30 20:07:43 -0500

    script/new-day --year 2021 2

--- src/years/y2021.rs
+++ src/years/y2021.rs
@@ -1,8 +1,12 @@
 mod d1;
+mod d02;
 
 use crate::solutionset::SolutionSet;
 
 pub fn register<S: SolutionSet>(s: &mut S) {
     s.add(2021, 1, 1, None, d1::part1);
     s.add(2021, 1, 2, None, d1::part2);
+
+    s.add(2021, 2, 1, None, d02::part1);
+    s.add(2021, 2, 2, None, d02::part2);
 }
