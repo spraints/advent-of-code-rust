bad938093148a1aaf3256d1647ad006735c42050
Matt Burke <spraints@gmail.com>
2023-12-01 23:57:13 -0500

    new-day 2

--- src/years/y2023.rs
+++ src/years/y2023.rs
@@ -1,4 +1,5 @@
 mod d01;
+mod d02;
 
 use crate::solutionset::SolutionSet;
 
@@ -7,4 +8,6 @@ pub fn register<S: SolutionSet>(s: &mut S) {
     s.add(2023, 1, 1, Some("orig"), d01::orig_part1);
     s.add(2023, 1, 2, None, d01::part2);
     s.add(2023, 1, 2, Some("orig"), d01::orig_part2);
+    s.add(2023, 2, 1, None, d02::part1);
+    s.add(2023, 2, 2, None, d02::part2);
 }
