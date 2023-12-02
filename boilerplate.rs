b913ea468b3f94267c96b6bd81a40bdbaaaec1bc
Matt Burke <spraints@gmail.com>
2023-12-02 10:28:15 -0500

    easy to get to docs

--- boilerplate.rs
+++ boilerplate.rs
@@ -1,5 +1,10 @@
 use std::fmt::Display;
 
+// Handy references:
+// - https://doc.rust-lang.org/std/iter/trait.Iterator.html
+// - https://docs.rs/itertools/0.8.2/itertools/trait.Itertools.html
+// - https://docs.rs/regex/latest/regex/struct.Regex.html
+
 pub fn part1(_input: String, _vis: bool) -> Box<dyn Display> {
     Box::new("todo")
 }
