de638b7c6b478c2929849e012955c8fb50861ee9
Matt Burke <spraints@gmail.com>
2023-12-06 08:55:22 -0500

    part 2

--- src/years/y2023/d06.rs
+++ src/years/y2023/d06.rs
@@ -7,48 +7,52 @@ use std::iter::zip;
 // - https://docs.rs/regex/latest/regex/struct.Regex.html
 
 pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
-    fn score(race: &Race, vis: bool) -> u64 {
-        // speed = x
-        // dist = speed * (time - x)
-        // break even when 0 = speed * (time - x) - best
-        // y = x * (time - x) - best
-        // y = x * time - x * x - best
-        // y = -x^2 + time * x - best
-        // a = -1
-        // b = time
-        // c = -best
-        // x = (-b +/- sqrt(b^2 - 4ac)) / 2a
-        let a = -1 as f64;
-        let b = race.time as f64;
-        let c = -1.0 * (race.best as f64);
-        let s = (b * b - 4.0 * a * c).sqrt();
-        let x1 = (-b + s) / 2.0 * a;
-        let x2 = (-b - s) / 2.0 * a;
-        if vis {
-            println!("0 = {a} * x^2 + {b} * x + {c}");
-            println!("time={} best={} => [ {x1}, {x2} ]", race.time, race.best);
-            //for i in 1..race.time {
-            //    println!("  press={i} ===> {}", i * (race.time - i));
-            //}
-        }
-        let mut x1 = x1.ceil() as u64;
-        if x1 * (race.time - x1) <= race.best {
-            x1 += 1;
-        }
-        let mut x2 = x2.floor() as u64;
-        if x2 * (race.time - x2) <= race.best {
-            x2 -= 1;
-        }
-        1 + x2 - x1
-    }
-
     let races = parse(&input);
     let res: u64 = races.iter().map(|r| score(r, vis)).product();
     Box::new(res)
 }
 
-pub fn part2(_input: String, _vis: bool) -> Box<dyn Display> {
-    Box::new("todo")
+pub fn part2(input: String, vis: bool) -> Box<dyn Display> {
+    let sp = regex::Regex::new(" ").unwrap();
+    let input = sp.replace_all(&input, "");
+    let races = parse(&input);
+    let res: u64 = races.iter().map(|r| score(r, vis)).product();
+    Box::new(res)
+}
+
+fn score(race: &Race, vis: bool) -> u64 {
+    // speed = x
+    // dist = speed * (time - x)
+    // break even when 0 = speed * (time - x) - best
+    // y = x * (time - x) - best
+    // y = x * time - x * x - best
+    // y = -x^2 + time * x - best
+    // a = -1
+    // b = time
+    // c = -best
+    // x = (-b +/- sqrt(b^2 - 4ac)) / 2a
+    let a = -1 as f64;
+    let b = race.time as f64;
+    let c = -1.0 * (race.best as f64);
+    let s = (b * b - 4.0 * a * c).sqrt();
+    let x1 = (-b + s) / 2.0 * a;
+    let x2 = (-b - s) / 2.0 * a;
+    if vis {
+        println!("0 = {a} * x^2 + {b} * x + {c}");
+        println!("time={} best={} => [ {x1}, {x2} ]", race.time, race.best);
+        //for i in 1..race.time {
+        //    println!("  press={i} ===> {}", i * (race.time - i));
+        //}
+    }
+    let mut x1 = x1.ceil() as u64;
+    if x1 * (race.time - x1) <= race.best {
+        x1 += 1;
+    }
+    let mut x2 = x2.floor() as u64;
+    if x2 * (race.time - x2) <= race.best {
+        x2 -= 1;
+    }
+    1 + x2 - x1
 }
 
 fn parse(input: &str) -> Vec<Race> {
@@ -80,5 +84,5 @@ mod test {
 Distance:  9  40  200";
 
     crate::test::aoc_test!(part1, TEST_INPUT, 288);
-    crate::test::aoc_test!(part2, TEST_INPUT, "todo");
+    crate::test::aoc_test!(part2, TEST_INPUT, 71503);
 }
