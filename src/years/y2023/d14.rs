9b7a492994ad86ae5bd182a1511554e5ad578146
Matt Burke <spraints@gmail.com>
2023-12-15 21:46:54 -0500

    part 2!

--- src/years/y2023/d14.rs
+++ src/years/y2023/d14.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fmt::Display;
 
 // Handy references:
@@ -6,13 +7,69 @@ use std::fmt::Display;
 // - https://docs.rs/regex/latest/regex/struct.Regex.html
 
 pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
-    let mut platform: Vec<Vec<char>> = input
-        .lines()
-        .map(|line| line.trim().chars().collect())
-        .collect();
-    let height = platform.len();
-    let width = platform[0].len();
+    let g = parse(&input);
 
+    let g = tilt_north(g);
+
+    if vis {
+        for line in &g.platform {
+            let line: String = line.iter().collect();
+            println!("{line}");
+        }
+    }
+
+    Box::new(load(g))
+}
+
+pub fn part2(input: String, _vis: bool) -> Box<dyn Display> {
+    let mut g = parse(&input);
+
+    const CYCLES: usize = 1000000000;
+    let mut positions = HashMap::new();
+    let mut i = 0;
+    let mut found = false;
+    loop {
+        i += 1;
+        g = tilt_north(g);
+        g = tilt_west(g);
+        g = tilt_south(g);
+        g = tilt_east(g);
+        if !found {
+            match positions.get(&g.platform).cloned() {
+                None => {
+                    positions.insert(g.platform.clone(), i);
+                }
+                Some(j) => {
+                    let repeat_interval = i - j;
+                    i += repeat_interval * ((CYCLES - i) / repeat_interval);
+                    found = true;
+                }
+            };
+        }
+        if i < CYCLES {
+            continue;
+        } else if i == CYCLES {
+            return Box::new(load(g));
+        } else {
+            panic!("huh?!?! {i} vs {CYCLES}");
+        }
+    }
+}
+
+fn load(g: Parsed) -> usize {
+    g.platform
+        .iter()
+        .enumerate()
+        .map(|(i, line)| (g.height - i) * (line.iter().filter(|c| matches!(c, 'O')).count()))
+        .sum()
+}
+
+fn tilt_north(g: Parsed) -> Parsed {
+    let Parsed {
+        mut platform,
+        height,
+        width,
+    } = g;
     let mut moved = true;
     while moved {
         moved = false;
@@ -26,24 +83,109 @@ pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
             }
         }
     }
-
-    if vis {
-        for line in &platform {
-            let line: String = line.iter().collect();
-            println!("{line}");
-        }
+    Parsed {
+        platform,
+        height,
+        width,
     }
-
-    let load: usize = platform
-        .iter()
-        .enumerate()
-        .map(|(i, line)| (height - i) * (line.iter().filter(|c| matches!(c, 'O')).count()))
-        .sum();
-    Box::new(load)
 }
 
-pub fn part2(_input: String, _vis: bool) -> Box<dyn Display> {
-    Box::new("todo")
+fn tilt_south(g: Parsed) -> Parsed {
+    let Parsed {
+        mut platform,
+        height,
+        width,
+    } = g;
+    let mut moved = true;
+    while moved {
+        moved = false;
+        for i in (1..height).rev() {
+            for j in 0..width {
+                if matches!((platform[i - 1][j], platform[i][j]), ('O', '.')) {
+                    moved = true;
+                    platform[i - 1][j] = '.';
+                    platform[i][j] = 'O';
+                }
+            }
+        }
+    }
+    Parsed {
+        platform,
+        height,
+        width,
+    }
+}
+
+fn tilt_west(g: Parsed) -> Parsed {
+    let Parsed {
+        mut platform,
+        height,
+        width,
+    } = g;
+    let mut moved = true;
+    while moved {
+        moved = false;
+        for j in 1..width {
+            for i in 0..height {
+                if matches!((platform[i][j - 1], platform[i][j]), ('.', 'O')) {
+                    moved = true;
+                    platform[i][j - 1] = 'O';
+                    platform[i][j] = '.';
+                }
+            }
+        }
+    }
+    Parsed {
+        platform,
+        height,
+        width,
+    }
+}
+
+fn tilt_east(g: Parsed) -> Parsed {
+    let Parsed {
+        mut platform,
+        height,
+        width,
+    } = g;
+    let mut moved = true;
+    while moved {
+        moved = false;
+        for j in (1..width).rev() {
+            for i in 0..height {
+                if matches!((platform[i][j - 1], platform[i][j]), ('O', '.')) {
+                    moved = true;
+                    platform[i][j - 1] = '.';
+                    platform[i][j] = 'O';
+                }
+            }
+        }
+    }
+    Parsed {
+        platform,
+        height,
+        width,
+    }
+}
+
+fn parse(input: &str) -> Parsed {
+    let platform: Vec<Vec<char>> = input
+        .lines()
+        .map(|line| line.trim().chars().collect())
+        .collect();
+    let height = platform.len();
+    let width = platform[0].len();
+    Parsed {
+        platform,
+        width,
+        height,
+    }
+}
+
+struct Parsed {
+    platform: Vec<Vec<char>>,
+    height: usize,
+    width: usize,
 }
 
 #[cfg(test)]
@@ -60,5 +202,5 @@ O.#..O.#.#
 #OO..#....";
 
     crate::test::aoc_test!(part1, TEST_INPUT, 136);
-    crate::test::aoc_test!(part2, TEST_INPUT, "todo");
+    crate::test::aoc_test!(part2, TEST_INPUT, 64);
 }
