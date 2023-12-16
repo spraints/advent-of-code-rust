480914290f06cbd3148937f854d908f08f6ab611
Matt Burke <spraints@gmail.com>
2023-12-15 22:00:38 -0500

    part 1 - 517625 is too high!

--- src/years/y2023/d15.rs
+++ src/years/y2023/d15.rs
@@ -5,18 +5,45 @@ use std::fmt::Display;
 // - https://docs.rs/itertools/0.8.2/itertools/trait.Itertools.html
 // - https://docs.rs/regex/latest/regex/struct.Regex.html
 
-pub fn part1(_input: String, _vis: bool) -> Box<dyn Display> {
-    Box::new("todo")
+pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
+    Box::new(
+        input
+            .split(',')
+            .map(|step| {
+                let h = hash(step.as_bytes(), false);
+                if vis {
+                    println!("{step} -> {h}");
+                }
+                h as u64
+            })
+            .sum::<u64>(),
+    )
 }
 
 pub fn part2(_input: String, _vis: bool) -> Box<dyn Display> {
     Box::new("todo")
 }
 
+fn hash(input: &[u8], vis: bool) -> u8 {
+    let mut current_value: u8 = 0;
+    for c in input {
+        current_value = current_value.wrapping_add(*c).wrapping_mul(17);
+        if vis {
+            println!("--> {c} --> {current_value}");
+        }
+    }
+    current_value
+}
+
 #[cfg(test)]
 mod test {
-    const TEST_INPUT: &'static str = r"";
+    const TEST_INPUT: &'static str = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
 
-    crate::test::aoc_test!(part1, TEST_INPUT, "todo");
+    crate::test::aoc_test!(part1, TEST_INPUT, 1320);
     crate::test::aoc_test!(part2, TEST_INPUT, "todo");
+
+    #[test]
+    fn test_hash() {
+        assert_eq!(super::hash("HASH".as_bytes(), true), 52);
+    }
 }
