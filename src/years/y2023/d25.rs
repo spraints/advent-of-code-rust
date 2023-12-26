d478df7863da8beb0eb504ed545ede0ff94c88dd
Matt Burke <spraints@gmail.com>
2023-12-26 08:27:00 -0500

    Karger-Stein algorithm does the trick, but the string comparisons are too slow

--- src/years/y2023/d25.rs
+++ src/years/y2023/d25.rs
@@ -1,10 +1,17 @@
 use std::collections::HashSet;
 use std::fmt::Display;
 
+use num::rational::Ratio;
+use rand::rngs::ThreadRng;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
 // Handy references:
 // - https://doc.rust-lang.org/std/iter/trait.Iterator.html
 // - https://docs.rs/itertools/0.8.2/itertools/trait.Itertools.html
 // - https://docs.rs/regex/latest/regex/struct.Regex.html
+
+const NODE_LABEL_LEN: usize = 3;
 
 pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
     let g = parse(&input);
@@ -13,13 +20,78 @@ pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
         println!("{} edges", g.edges.len());
     }
 
-    Box::new("todo")
+    let inv_sqrt_2 = Ratio::new(99, 70).recip();
+    let mut rng = thread_rng();
+
+    let g = fastmincut(g, &inv_sqrt_2, &mut rng);
+    if vis {
+        println!("final nodes = {:?}", g.nodes);
+        println!("final edges = {:?}", g.edges);
+    }
+    assert_eq!(2, g.nodes.len());
+    assert_eq!(3, g.edges.len());
+    let a = g.nodes[0].len() / NODE_LABEL_LEN;
+    let b = g.nodes[1].len() / NODE_LABEL_LEN;
+    Box::new(a * b)
 }
 
 pub fn part2(_input: String, _vis: bool) -> Box<dyn Display> {
     Box::new("todo")
 }
 
+// Karger-Stein algorithm.
+// https://en.wikipedia.org/wiki/Karger%27s_algorithm#Karger%E2%80%93Stein_algorithm
+fn fastmincut(g: Graph, inv_sqrt_2: &Ratio<usize>, rng: &mut ThreadRng) -> Graph {
+    if g.nodes.len() <= 6 {
+        contract(g, 2, rng)
+    } else {
+        let t = (inv_sqrt_2 * g.nodes.len() + 1).ceil().to_integer();
+        let g1 = fastmincut(contract(g.clone(), t, rng), inv_sqrt_2, rng);
+        let g2 = fastmincut(contract(g, t, rng), inv_sqrt_2, rng);
+        if g1.edges.len() < g2.edges.len() {
+            g1
+        } else {
+            g2
+        }
+    }
+}
+
+fn contract(g: Graph, t: usize, rng: &mut ThreadRng) -> Graph {
+    let Graph {
+        mut nodes,
+        mut edges,
+    } = g;
+    while nodes.len() > t {
+        let (from, to) = edges.choose(rng).unwrap();
+        let from = from.to_string();
+        let to = to.to_string();
+        let combined = format!("{from}{to}");
+        nodes = nodes
+            .into_iter()
+            .filter_map(|node| match node {
+                node if node == from => Some(combined.clone()),
+                node if node == to => None,
+                node => Some(node),
+            })
+            .collect();
+        edges = edges
+            .into_iter()
+            .filter_map(|edge| match edge {
+                (a, b) if a == from && b == to => None,
+                (a, b) if b == from && a == to => None,
+                (a, b) if a == from || a == to => Some((combined.clone(), b)),
+                (a, b) if b == from || b == to => Some((a, combined.clone())),
+                (a, b) => Some((a, b)),
+            })
+            .collect();
+        //println!("DELETEME: --------");
+        //println!("DELETEME: nodes = {nodes:?}");
+        //println!("DELETEME: edges = {edges:?}");
+    }
+    Graph { nodes, edges }
+}
+
+#[derive(Clone)]
 struct Graph {
     nodes: Vec<String>,
     edges: Vec<(String, String)>,
@@ -30,8 +102,10 @@ fn parse(input: &str) -> Graph {
     let mut edges = Vec::new();
     for line in input.lines() {
         let (from, tos) = line.trim().split_once(": ").unwrap();
+        assert_eq!(NODE_LABEL_LEN, from.len());
         nodes.insert(from.to_owned());
         for to in tos.split_whitespace() {
+            assert_eq!(NODE_LABEL_LEN, to.len());
             nodes.insert(to.to_owned());
             edges.push((from.to_owned(), to.to_owned()));
         }
