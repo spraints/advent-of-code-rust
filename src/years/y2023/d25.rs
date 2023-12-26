526bd3f61925edfb19b571d0953ac5321bb9459b
Matt Burke <spraints@gmail.com>
2023-12-25 22:20:20 -0500

    Kernighan-Lin Algorithm won't work because it doesn't change the size of the subgraphs
    Also, my implementation is bad, it doesn't find the same minimum set
    every time.

--- src/years/y2023/d25.rs
+++ src/years/y2023/d25.rs
@@ -1,4 +1,4 @@
-use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::collections::{HashMap, HashSet};
 use std::fmt::Display;
 
 // Handy references:
@@ -18,15 +18,17 @@ pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
     let mut v2 = HashSet::new();
     for (i, n) in nodes.iter().enumerate() {
         if i % 2 == 0 {
-            v1.insert(n);
+            v1.insert(n.to_string());
         } else {
-            v2.insert(n);
+            v2.insert(n.to_string());
         }
     }
     // Calculate internal and external "costs" for each node.
     // c(a,b) = 1 if connected or 0 otherwise.
     // E(a) = sum(c(a, v) for v in V(2) (when a is in V(1)).
     // I(a) = sum(c(a, v) for v in V(1) (when a is in V(1)).
+    // This implementation is a shortcut because we can skip all the pairs where we know there
+    // isn't an edge.
     let mut ext_costs = HashMap::new();
     let mut int_costs = HashMap::new();
     for (n, tos) in &edges {
@@ -41,6 +43,19 @@ pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
             *e += 1;
         }
     }
+    if vis {
+        println!("INITIAL COSTS");
+        for n in &nodes {
+            println!("{n}: I={:?} E={:?}", int_costs.get(n), ext_costs.get(n));
+        }
+    }
+    // Calculate c(a,b) for every pair. This will simplify things later.
+    let mut cabs: HashMap<(&str, &str), i32> = HashMap::new();
+    for (from, tos) in &edges {
+        for to in tos {
+            cabs.insert((from, to), 1);
+        }
+    }
     // D(a) = E(a) - I(i)
     let mut d = HashMap::new();
     for n in &nodes {
@@ -53,11 +68,11 @@ pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
     loop {
         if vis {
             println!("---ITERATION---");
-            println!("V1 = {v1:?}");
-            println!("V2 = {v2:?}");
+            println!("V1 = {:?}", sorted(&v1));
+            println!("V2 = {:?}", sorted(&v2));
         }
-        // g(a,b) = D(a) + D(b) - 2*c(a, b)
-        let mut g = BinaryHeap::new();
+        // Find maximum: g(a,b) = D(a) + D(b) - 2*c(a, b)
+        let mut best = None;
         for n1 in &v1 {
             if locked.contains(n1) {
                 continue;
@@ -66,25 +81,51 @@ pub fn part1(input: String, vis: bool) -> Box<dyn Display> {
                 if locked.contains(n2) {
                     continue;
                 }
-                let cab = match edges.get(*n1).map(|tos| tos.contains(n2)) {
-                    Some(true) => 1,
-                    _ => 0,
-                };
+                let cab = cabs.get(&(n1, n2)).copied().unwrap_or(0);
                 let gab = d.get(n1).unwrap() + d.get(n2).unwrap() - 2 * cab;
-                g.push((gab, cab, n1, n2));
+                if best.is_none() || matches!(best, Some((n, _, _)) if gab > n) {
+                    best = Some((gab, n1, n2));
+                }
             }
         }
-        match g.pop() {
-            None => break,
-            Some((gab, cab, a, b)) => {
-                if vis {
-                    println!("best: {a} vs {b} {gab} ({cab})");
+        if vis {
+            println!("best: {best:?}");
+        }
+        match best {
+            Some((gab, a, b)) if gab > 0 => {
+                let a = a.clone();
+                let b = b.clone();
+                assert!(v1.remove(&a));
+                assert!(v2.remove(&b));
+                for x in &v1 {
+                    let dx = d.get_mut(x).unwrap();
+                    let cxa = cabs.get(&(x, &a)).copied().unwrap_or(0);
+                    let cxb = cabs.get(&(x, &b)).copied().unwrap_or(0);
+                    *dx += 2 * cxa - 2 * cxb;
                 }
+                for y in &v2 {
+                    let dy = d.get_mut(y).unwrap();
+                    let cyb = cabs.get(&(y, &b)).copied().unwrap_or(0);
+                    let cya = cabs.get(&(y, &a)).copied().unwrap_or(0);
+                    *dy += 2 * cyb - 2 * cya;
+                }
+                v1.insert(b.clone());
+                v2.insert(a.clone());
                 locked.insert(a);
                 locked.insert(b);
-                if locked.len() > 0 {
-                    // todo replace this with a swap
-                    break;
+            }
+            _ => break,
+        }
+    }
+
+    if vis {
+        println!("---FINISHED!---");
+        println!("V1 = {:?}", sorted(&v1));
+        println!("V2 = {:?}", sorted(&v2));
+        for a in &v1 {
+            for b in &v2 {
+                if cabs.get(&(a, b)).is_some() {
+                    println!("{a} connects to {b}");
                 }
             }
         }
@@ -116,6 +157,12 @@ fn parse(input: &str) -> (HashSet<String>, HashMap<String, Vec<String>>) {
         }
     }
     (nodes, edges)
+}
+
+fn sorted(v: &HashSet<String>) -> Vec<String> {
+    let mut res: Vec<String> = v.iter().cloned().collect();
+    res.sort();
+    res
 }
 
 #[cfg(test)]
